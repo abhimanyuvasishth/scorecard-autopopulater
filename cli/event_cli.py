7955d56dbc94f5bf540e32ff0b565ffdb1b78bf6
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2022-07-08 15:17:29 -0500

    event uses just sheet

--- cli/event_cli.py
+++ cli/event_cli.py
@@ -3,7 +3,6 @@ import click
 from cli import logger
 from scorecard_autopopulater.generator.match_generator import MatchGenerator
 from scorecard_autopopulater.google_sheet import GoogleSheet
-from scorecard_autopopulater.writer.cricket_sheet_writer import CricketSheetWriter
 
 
 @click.group(name='event_cli')
@@ -14,14 +13,14 @@ def event_cli():
 @event_cli.command(name='process_current_matches',)
 @click.option('--dry-run', type=bool, is_flag=True, help='dry run updates')
 def process_current_matches(dry_run):
-    writer = CricketSheetWriter(GoogleSheet('Sandbox', 'Sandbox'))
+    sheet = GoogleSheet('Sandbox', 'Sandbox')
     for match in MatchGenerator().generate_matches(limit=10):
         logger.info({'match': [match.id, match.series_id, match.stage, match.format]})
         for team in match.teams:
             logger.info({'team': [team.id, team.long_name]})
             for player in team.players:
                 if not dry_run:
-                    writer.write_data_item(player, team)
+                    sheet.write_data_item(player, team)
                 logger.info({'player': player})
         logger.info(f'Completed logging {match.id}')
 
