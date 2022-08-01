b05ba0b264f5e5c14599245320d1af02b855d25a
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2022-08-01 00:46:25 -0700

    added tournament id filter

--- cli/event_cli.py
+++ cli/event_cli.py
@@ -3,7 +3,7 @@ from time import sleep
 import click
 
 from cli import logger
-from scorecard_autopopulater.generator.cricket_match_generator import CricketMatchGenerator
+from scorecard_autopopulater.cricket_match_generator import generate_matches
 from scorecard_autopopulater.google_sheet import GoogleSheet
 
 
@@ -12,11 +12,15 @@ def event_cli():
     pass
 
 
-@event_cli.command(name='process_current_matches',)
+@event_cli.command(name='process_current_matches')
 @click.option('--dry-run', type=bool, is_flag=True, help='dry run updates')
-def process_current_matches(dry_run):
+@click.option('--tournament_id', type=int, help='tournament id to scrape')
+def process_current_matches(dry_run, tournament_id):
     sheet = GoogleSheet('Sandbox', 'Sandbox')
-    for match in CricketMatchGenerator().generate_matches(limit=5):
+    for match in generate_matches():
+        if tournament_id and match.tournament_id != tournament_id:
+            continue
+        match.populate()
         logger.info(match)
         for team in match.teams:
             logger.info(team)
