0c73a3c651a7fccb9efe7b49c720a785c58e9144
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2023-03-31 20:33:05 -0700

    cricket cli

--- job.sh
+++ job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=.
-source "$HOME"/miniconda3/bin/activate scorecard-autopopulater && python cli/football_cli.py process_current_matches
+source "$HOME"/miniconda3/bin/activate scorecard-autopopulater && python cli/cricket_cli.py process_current_matches
